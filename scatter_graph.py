0605c563bdc029a4f0586114d57516ee018e3832
Kaliiiii <40872061+Kaliiiii@users.noreply.github.com>
2025-02-16 21:04:25 -0800

    Fixed X values, and plotting on scatter plot

--- scatter_graph.py
+++ scatter_graph.py
@@ -5,10 +5,9 @@ import helpers
 from constants_globals import *
 
 TITLE_TEXT = "Scatter Plot"
-SPACE_BETWEEN_Y_LABELS = 20  
+SPACE_BETWEEN_Y_LABELS = 20 
 DOT_SIZE = 5
 
-#TO DO: implement decimal values for hours
 def scatter_graph(d_list, turtle_obj):
     helpers.build_graph(turtle_obj)
 
@@ -16,26 +15,30 @@ def scatter_graph(d_list, turtle_obj):
     turtle_obj.goto(HOME_X, TITLE_Y)
     turtle_obj.write(TITLE_TEXT, font= TITLE_FONT)
 
-    label_space = GRAPH_WIDTH / len(d_list)
+    dictionary_values = defaultdict(list)
+    for x, y in d_list:
+        if x in dictionary_values.keys():
+            dictionary_values[x].append(y)
+        else:
+            dictionary_values[x] = [y]
+    print(dictionary_values)
+    print (dictionary_values.keys())
 
-    x_list = []
-    y_list = []
-    for val in d_list:
-        x_list.append(val[0])
-        y_list.append(val[1])
-    
-    labels = list(range(0, SP_X_MAX))
+    label_space = GRAPH_WIDTH / len(dictionary_values.keys())
+    labels = list(dictionary_values.keys())
+    print(labels)
+    labels.sort()
+    print(labels)
     helpers.write_x_labels(turtle_obj, labels, False, label_space)
-    helpers.write_y_labels(turtle_obj, SPACE_BETWEEN_Y_LABELS, SP_Y_MAX, 3)
+    helpers.write_y_labels(turtle_obj, SPACE_BETWEEN_Y_LABELS, SP_Y_MAX, 1)
 
     turtle_obj.penup()
     turtle_obj.goto(HOME_X, HOME_Y)
     pos_diff = label_space
-    for x in labels:
-        if x in x_list:
-            ind = x_list.index(x)
-            turtle_obj.goto(HOME_X+pos_diff, HOME_Y+y_list[ind])
+    for key in labels:
+        for val in dictionary_values[key]:
+            turtle_obj.goto(HOME_X+pos_diff, HOME_Y+val)
             turtle_obj.dot(DOT_SIZE)
         pos_diff += label_space
-    
+
     turtle_obj.hideturtle()
